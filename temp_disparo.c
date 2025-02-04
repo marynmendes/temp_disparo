48debc0efda3abeb464ba5380019ae2c79ed8648
marynmendes <marynicolesm@gmail.com>
2025-02-04 17:57:08 -0300

    Adição da biblioteca pico/time, definição dos pinos dos leds e do botão e criação de variáveis para acompanhar a situação dos leds

--- temp_disparo.c
+++ temp_disparo.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/time.h" //biblioteca para gerenciar temprizador e alarme
+
+#define led_red 13 //luz vermelha
+#define led_blue 12 //led azul 
+#define led_green 11 // luz verde
+#define button_a 5 // botão A
+
+//variáveis para representar situação dos leds
+bool led_red_active = false;
+bool led_blue_active = false;
+bool led_green_active = false;
+
 
 
 
