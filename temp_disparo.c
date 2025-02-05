d569682fd60a39cfec7c60da166d01d9a6131ec6
marynmendes <marynicolesm@gmail.com>
2025-02-04 23:03:27 -0300

    Correção geral do código com implementação de interrupção

--- temp_disparo.c
+++ temp_disparo.c
@@ -1,26 +1,47 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "pico/time.h" //biblioteca para gerenciar temprizador e alarme
+#include "pico/time.h" 
+#include "hardware/gpio.h"
+#include "hardware/timer.h" //bibliotecas para gerenciar temprizador e alarme
 
 #define led_red 13 //luz vermelha
 #define led_blue 12 //led azul 
 #define led_green 11 // luz verde
 #define button_a 5 // botão A
 
-//variáveis para representar situação dos leds
-bool led_red_active = false;
-bool led_blue_active = false;
-bool led_green_active = false;
-
-//função após o fim da rotina dos leds
-int64_t turn_off_callback(alarm_id_t id, void *user_data){
-        
-        led_red_active = false;
-        led_blue_active = false;
-        led_green_active = false;
+//funções após o fim da rotina dos leds
+int64_t turn_off_callback_green(alarm_id_t id, void *user_data){
+    gpio_put(led_green, false);
     
     return 0;
 }
+
+int64_t turn_off_callback_blue(alarm_id_t id, void *user_data){
+    gpio_put(led_blue, false);
+    add_alarm_in_ms(3000, turn_off_callback_green, NULL, false);
+    return 0;
+}
+
+int64_t turn_off_callback_red(alarm_id_t id, void *user_data){
+    gpio_put(led_red, false);
+    add_alarm_in_ms(3000, turn_off_callback_blue, NULL, false);
+    return 0;
+}
+
+//função que ligará os leds pelo tempo determinado
+void acionar(uint gpio, uint32_t eventos){
+    if (gpio_get(led_red) != 0 || gpio_get(led_blue) != 0 || gpio_get(led_green) != 0){
+        return;
+    } else { 
+    gpio_put(led_red, true);
+    gpio_put(led_blue, true);
+    gpio_put(led_green, true);
+
+    add_alarm_in_ms(3000, turn_off_callback_red, NULL, false);
+    }
+    
+}
+
 
 int main()
 {
@@ -36,44 +57,22 @@ int main()
     gpio_set_dir(led_blue, true);
     gpio_set_dir(led_green, true);
 
+    //leds desligados
+    gpio_put(led_red, false);
+    gpio_put(led_blue, false);
+    gpio_put(led_green, false);
+
     //configuração do botão como entrada
-    gpio_set_dir(button_a, GPIO_IN);
+    gpio_set_dir(button_a, false);
 
     //habilita o resistor pull-up interno para o pino do botão.
     gpio_pull_up(button_a);
 
+    //chama função de interrupção
+    gpio_set_irq_enabled_with_callback(button_a, GPIO_IRQ_EDGE_FALL, true, acionar);
+
     while (true) {
-         // Verifica se o botão foi pressionado (nível baixo no pino) e se o LED não está ativo.
-        if (gpio_get(button_a) == 0 && !(led_red_active == false && led_blue_active == false && led_green_active == false)) {
-            sleep_ms(50);
-            // Verifica novamente o estado do botão após o debounce.
-            if (gpio_get(button_a) == 0) {
-                //condições para acionamento de leds
-                if(led_red_active == false && led_blue_active == false && led_green_active == false){
-                    gpio_put(led_red, true);
-                    gpio_put(led_blue, true);
-                    gpio_put(led_green, true);
-
-                    led_red_active = true;
-                    led_blue_active = true;
-                    led_green_active = true;
-                } else if (led_red_active == true && led_blue_active == true && led_green_active == true){
-                    gpio_put(led_red, false);
         
-                    led_red_active = false;
-                } else if (led_red_active == false && led_blue_active == true && led_green_active == true){
-                    gpio_put(led_blue, false);
-
-                    led_blue_active = false;
-                } else if (led_red_active == false && led_blue_active == false && led_green_active == true){
-                    gpio_put(led_green, false);
-
-                    led_green_active = false;
-                }
-                
-                add_alarm_in_ms(3000, turn_off_callback, NULL, false);
-            }
-        }
     }
 
 }
